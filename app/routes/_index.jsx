0031935d135a11261a404e10123af577f7873254
Magnus Kingo <magnus@metteogivar.dk>
2024-11-02 16:58:33 +0100

    Added logout button and hello user text!

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,5 +1,5 @@
-import { getSession } from "../services/session.server.jsx";
-import { redirect } from "@remix-run/react";
+import { getSession, destroySession } from "../services/session.server.jsx";
+import { redirect, useLoaderData, Form } from "@remix-run/react";
 
 export async function loader({ request }) {
   const session = await getSession(request.headers.get("Cookie"));
@@ -9,4 +9,24 @@ export async function loader({ request }) {
   return session.data;
 }
 
-export default function index() {}
+export default function Index() {
+  const sessionData = useLoaderData();
+  console.log(sessionData);
+  return (
+    <div>
+      <h1>Welcome, {sessionData.username}!</h1>
+      <Form method="post">
+        <button type="submit">Logout</button>
+      </Form>
+    </div>
+  );
+}
+
+export async function action({ request }) {
+  const session = await getSession(request.headers.get("Cookie"));
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+}
