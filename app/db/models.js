c43797dc7a3496317ebe7ac9b3455b885884622a
MagnusKingoDanielsen <magnus@metteogivar.dk>
2024-11-11 16:51:25 +0100

    Added camp model

--- app/db/models.js
+++ app/db/models.js
@@ -35,10 +35,43 @@ const drejerSchema = new Schema(
   },
   { timestamps: true },
 );
+
+const campsSchema = new Schema({
+  CampName: {
+    type: String,
+    required: true,
+  },
+  StartDate: {
+    type: String,
+    required: true,
+  },
+  EndDate: {
+    type: String,
+    required: true,
+  },
+  CampLeader: {
+    type: String,
+    required: true,
+  },
+  CampDescription: {
+    type: String,
+    required: true,
+  },
+  Participants: {
+    type: String,
+    required: true,
+  },
+});
+
 export const models = [
   {
     name: "drejers",
     schema: drejerSchema,
     collection: "drejers",
+  },
+  {
+    name: "camps",
+    schema: campsSchema,
+    collection: "camps",
   },
 ];
