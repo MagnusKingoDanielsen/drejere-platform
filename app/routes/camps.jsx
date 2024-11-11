4fab1bc199d9cd55fe8ae481649a5a73d36d26bc
MagnusKingoDanielsen <magnus@metteogivar.dk>
2024-11-11 22:34:38 +0100

    Display camps

--- app/routes/camps.jsx
+++ app/routes/camps.jsx
@@ -1,16 +1,38 @@
-import { useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { redirect } from "@remix-run/react";
 import { getSession } from "../services/session.server.jsx";
+import mongoose from "mongoose";
 
 export async function loader({ request }) {
   const session = await getSession(request.headers.get("Cookie"));
   if (!session.data.user) {
     return redirect("/");
   }
-  return session.data;
+  const camps = await mongoose.models.camps.find().lean().exec();
+  return {
+    session: session.data,
+    camps: camps.map((camp) => ({
+      ...camp,
+      StartDate: new Date(camp.StartDate).toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      EndDate: new Date(camp.EndDate).toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    })),
+  };
 }
 
 export default function CampPage() {
+  const { camps } = useLoaderData();
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
@@ -20,6 +42,19 @@ export default function CampPage() {
   return (
     <div>
       <button onClick={handleButtonClick}>Create Camp</button>
+      <h1>List of Camps</h1>
+      <ul>
+        {camps.map((camp) => (
+          <li key={camp._id}>
+            <h2>{camp.CampName}</h2>
+            <p>Start Date: {new Date(camp.StartDate).toLocaleString()}</p>
+            <p>End Date: {new Date(camp.EndDate).toLocaleString()}</p>
+            <p>Leader: {camp.CampLeader}</p>
+            <p>Description: {camp.CampDescription}</p>
+            <p>Participants: {camp.Participants.join(", ")}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
