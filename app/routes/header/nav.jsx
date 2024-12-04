c19d4e8273252b25f0510b63491de23f163e5097
MagnusKingoDanielsen <magnus@metteogivar.dk>
2024-12-04 13:14:00 +0100

    Update nav.jsx

--- app/routes/header/nav.jsx
+++ app/routes/header/nav.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useLoaderData, Form } from "@remix-run/react";
 import {
-  RiPrinterLine,
+  // RiPrinterLine,
   RiAccountCircleLine,
   RiLogoutBoxRLine,
 } from "react-icons/ri";
@@ -43,9 +43,9 @@ export default function Nav() {
     setIsAdminMenuOpen(!isAdminMenuOpen);
   };
 
-  const handlePrint = () => {
-    window.print();
-  };
+  // const handlePrint = () => {
+  //   window.print();
+  // };
 
   if (!usertype) {
     return null;
@@ -79,11 +79,11 @@ export default function Nav() {
                     Drejerliste
                   </Link>
                 </li>
-                <li>
+                {/* <li>
                   <Link to="/noegleListe" onClick={() => setIsMenuOpen(false)}>
                     Nøgleliste
                   </Link>
-                </li>
+                </li> */}
                 {usertype === "admin" && (
                   <li className="dropdown">
                     <button className="dropbtn" onClick={toggleAdminMenu}>
@@ -113,11 +113,11 @@ export default function Nav() {
                     )}
                   </li>
                 )}
-                <li className="printerIcon">
+                {/* <li className="printerIcon">
                   <button onClick={handlePrint} className="printerIconButton">
                     <RiPrinterLine />
                   </button>
-                </li>
+                </li> */}
                 <li className="profileIcon">
                   <Link to="/profile" onClick={() => setIsMenuOpen(false)}>
                     <RiAccountCircleLine />
@@ -149,9 +149,9 @@ export default function Nav() {
             <li>
               <Link to="/drejerListe">Drejerliste</Link>
             </li>
-            <li>
+            {/* <li>
               <Link to="/noegleListe">Nøgleliste</Link>
-            </li>
+            </li> */}
             {usertype === "admin" && (
               <li className="dropdown">
                 <button className="dropbtn">Admin</button>
@@ -163,11 +163,11 @@ export default function Nav() {
                 </div>
               </li>
             )}
-            <li className="printerIcon">
+            {/* <li className="printerIcon">
               <button onClick={handlePrint} className="printerIconButton">
                 <RiPrinterLine />
               </button>
-            </li>
+            </li> */}
             <li className="profileIcon">
               <Link to="/profile">
                 <RiAccountCircleLine />
